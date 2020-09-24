beb6443d44b1f6e8dbec9bcba0ce443b806a2034
sree-chikati <srechi453.fusdk12@gmail.com>
2020-09-23 19:53:21 -0700

    what I have now doesn't work

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-const billInput = document.querySelector('#input-bill')
-const tipInput = document.querySelector('#input-tip')
-const tipInput = document.querySelector('#input-people')
-const display = document.querySelector('#display-tip')
-const display = document.querySelector('#display-total')
+const billInput = document.querySelector('#bill')
+const tipInput = document.querySelector('#tip')
+const peopleInput = document.querySelector('#people')
+const displayTip = document.querySelector('#tip')
+const displayTotal = document.querySelector('#total')
+
+billInput.addEventListener('input', handleBill)
+tipInput.addEventListener('input', handleBill)
+peopleInput.addEventListener('input', handleBill)
+
+function handleBill() {
+    const billValue = billInput.value
+    const tipValue = tipInput.value
+    
+    const percent = paresInt(tipInput.value)
+
+    const tip = bill * percent / 100
+    const total = bill + tip
+
+    displayTip.innerHTML = tip    
+    
+}
+handleBill()
